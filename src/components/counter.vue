222026dc77f262f68818b32985144c530a617bd3
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-19 15:43:59 +0800

    implement counter

---
+++ src/components/counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id = "counter">
+        <button v-on:click="decrease"> - </button>
+        <span> {{ count }} </span>
+        <button v-on:click="increase"> + </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "counter",
+        data() {
+            return {
+                count: 0,
+            }
+        },
+        methods: {
+            decrease(){
+              this.count --;
+            },
+            increase(){
+                this.count++;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
