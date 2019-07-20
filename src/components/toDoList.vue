216b73727c085661cb95eff1883ff16cc9014949
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-20 18:11:17 +0800

    upload to cloud.

--- src/components/toDoList.vue
+++ src/components/toDoList.vue
@@ -1,65 +1,33 @@
 <template>
-    <div id="todo-list-example">
-        <form v-on:submit.prevent="addNewTodo">
-            <label for="new-todo">Add a todo</label>
-            <input
-                    v-model="newTodoText"
-                    id="new-todo"
-                    placeholder="E.g. Feed the cat"
-            >
-            <button>Add</button>
-        </form>
-        <ul>
-            <li
-                    is="todo-item"
-                    v-for="(todo, index) in todos"
-                    v-bind:key="todo.id"
-                    v-bind:title="todo.title"
-                    v-on:remove="todos.splice(index, 1)"
-            ></li>
-        </ul>
+    <div id = "to-do-list" class="div1">
+       <div class="div2">
+           <h1>Jquery to do list</h1>
+           <p>simple Todo List with adding and filter by diff status.</p>
+           <input class="input1" v-model="newItem"/><button class="add" v-on:submit.prevent = "addNewItem">Add</button>
+            <br />
+           <span>{{newItem}}</span>
+           <div v-for="(index,name,value) in todos" v-bind:key="index" class="div3" @change="addNewItem">
+               {{index}}. {{name}}: <input type="checkbox" class="checkboxs">{{value}}
+           </div>
+       </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "toDoList",
-        component:('todo-item', {
-            template: '\
-<li>\
-    {{ title }}\
-    <button v-on:click="$emit(\'remove\')">Remove</button>\
-</li>\
-',
-            props: ['title']
-        }),
+        name : "to-do-list",
         data() {
             return{
-                newTodoText: '',
-                todos: [
-                    {
-                        id: 1,
-                        title: 'Do the dishes',
-                    },
-                    {
-                        id: 2,
-                        title: 'Take out the trash',
-                    },
-                    {
-                        id: 3,
-                        title: 'Mow the lawn'
-                    }
+                newItem: '',
+                // eslint-disable-next-line no-sparse-arrays
+                todos:  [
+                    'Do the dishes','Take out the trash' , 'Mow the lawn'
                 ],
-                nextTodoId: 4,
             }
         },
-        methods: {
-            addNewTodo: function () {
-                this.todos.push({
-                    id: this.nextTodoId++,
-                    title: this.newTodoText
-                })
-                this.newTodoText = ''
+        methods:{
+            addNewItem: function () {
+                this.todos.push(this.newItem)
             }
         }
 
@@ -67,5 +35,43 @@
 </script>
 
 <style scoped>
-
+    .div1 {
+        background-color: lightgrey;
+        padding: 20px;
+        height: 100%;
+        position: relative;
+    }
+    .div2 {
+        background-color: white;
+        border-radius: 5px;
+        padding: 30px;
+        width: 500px;
+        height: 500px;
+        margin: auto;
+    }
+    .add{
+        color: white;
+        background-color: lightcoral;
+        border: unset;
+        border-radius: 5px;
+        margin-left: 10px;
+        width: 80px;
+        height: 35px;
+        font-size: 20px;
+        float: left;
+    }
+    .input1{
+        float: left;
+        height: 30px;
+        width: 350px;
+    }
+    .div3{
+        margin-top: 20px;
+        padding: 10px;
+    }
+    .checkboxs{
+        width: 10px;
+        height: 10px;
+        float: left;
+    }
 </style>
