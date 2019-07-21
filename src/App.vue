2eaf212ded417442cb2289e2d64d0906fa6e608d
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-21 19:35:03 +0800

    create to-do-list 's model,implement add input test to be a list item

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <input placeholder="please input the counter number. " v-model = "counterNum"/>
-    <counter-group :counterNum = parseInt(counterNum)></counter-group>
+    <to-do-list></to-do-list>
   </div>
 </template>
 
 <script>
-import counterGroup from './components/counterGroup.vue'
+
+import toDoList from './components/toDoList.vue'
+
 export default {
   name: 'app',
   components: {
-    counterGroup,
+    toDoList,
   },
   data() {
     return{
-      counterNum : 0
+
     }
   },
   props(){
@@ -29,7 +30,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin: 60px;
 }
--- src/components/toDoList.vue
+++ src/components/toDoList.vue
@@ -1,55 +1,105 @@
 <template>
-    <div id = "to-do-list" class="div1">
-       <div class="div2">
-           <h1>Jquery to do list</h1>
-           <p>simple Todo List with adding and filter by diff status.</p>
-           <input class="input1" v-model="newItem"/><button class="add" v-on:submit.prevent = "addNewItem">Add</button>
-            <br />
-           <span>{{newItem}}</span>
-           <div v-for="(index,name,value) in todos" v-bind:key="index" class="div3" @change="addNewItem">
-               {{index}}. {{name}}: <input type="checkbox" class="checkboxs">{{value}}
-           </div>
-       </div>
+    <div id="to-do-list" class="div1">
+        <div class="div2">
+            <h1>Jquery to do list</h1>
+            <em>simple Todo List with adding and filter by diff status.</em>
+            <br/>
+            <input class="input1" type="text" v-model="newItem"
+                   placeholder="please enter your new item^_^"
+                   v-on:keyup.enter="addNewItem"/>
+            <button class="add" v-on:click="addNewItem">Add</button>
+            <br/><br/>
+
+            <div>
+                <div v-for="(item,index) in returnList()" v-bind:key="item.id"
+                     class="div3" v-on:change="addNewItem">
+                    <span v-if="item.addToComplete">{{ index++ }}.</span>
+                    <span v-else> {{ index++ }}.</span>
+                    <div class="listShowDiv">
+                        <div class="listShow">
+                            <div v-if="item.removeItem" class="completedItem">
+                                <input type="text" v-model="newItem"
+                                       v-on:keyup.enter="addToComplete(item)">
+                            </div>
+                            <div v-else class="notCompletedItem">
+                                <input type="checkbox" v-model="item.completed"
+                                       class="input2"
+                                       v-on:click="setItemCompleted(item)">
+                                <span v-if="item.completed"
+                                      class="toDoCompleted">{{item.data}}</span>
+                                <span v-else v-on:dblclick="setItemActive(item)">{{item.data}}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-if="!item.removeItem"
+                         v-on:click="removeToDo(item)"
+                         class="toDoDeleteDiv">
+                        <span>&times;</span>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div class="selectButton">
+                    <a href="#" v-on:click="filterByCondition('ALL')">ALL</a>
+                    <a href="#" v-on:click="filterByCondition('ACTIVE')">ACTIVE</a>
+                    <a href="#" v-on:click="filterByCondition('COMPLETE')">COMPLETE</a>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name : "to-do-list",
+        name: "to-do-list",
         data() {
-            return{
+            return {
+                listIndex: 1,
                 newItem: '',
-                // eslint-disable-next-line no-sparse-arrays
-                todos:  [
-                    'Do the dishes','Take out the trash' , 'Mow the lawn'
-                ],
+                currentItem: '',
+                showList: [],
+                filterCondition: "ALL"
             }
         },
-        methods:{
+        methods: {
             addNewItem: function () {
-                this.todos.push(this.newItem)
-            }
-        }
-
+                if (this.newItem.trim().length === 0) {
+                    return
+                }
+                this.showList.push({
+                    id: this.listIndex,
+                    data: this.newItem,
+                    completed: false,
+                    removeItem: false
+                }),
+                    this.newItem = '',
+                    this.listIndex++
+            },
+            
+        },
     }
 </script>
 
 <style scoped>
     .div1 {
-        background-color: lightgrey;
         padding: 20px;
         height: 100%;
         position: relative;
     }
+
     .div2 {
         background-color: white;
         border-radius: 5px;
+        border: #42b983 20px solid;
         padding: 30px;
         width: 500px;
-        height: 500px;
+        height: auto;
         margin: auto;
     }
-    .add{
+
+    .add {
         color: white;
         background-color: lightcoral;
         border: unset;
@@ -60,18 +110,61 @@
         font-size: 20px;
         float: left;
     }
-    .input1{
+
+    .input1 {
         float: left;
         height: 30px;
         width: 350px;
+        font-size: 25px;
     }
-    .div3{
+
+    .div3 {
         margin-top: 20px;
         padding: 10px;
+        border-radius: 5px;
+        font-size: 25px;
     }
-    .checkboxs{
-        width: 10px;
-        height: 10px;
-        float: left;
+
+    .div3:nth-child(odd) {
+        background-color: lightcoral;
+    }
+
+    .listShowDiv {
+        display: inline-block;
+        text-align: left;
+        width: 350px;
+    }
+
+    .listShow {
+        display: inline-block;
+    }
+
+    .selectButton {
+        margin-bottom: 10px;
+        padding-top: 20px;
+    }
+
+    .selectButton a {
+        text-decoration: none;
+        outline: none;
+        color: darksalmon;
+        margin: 15px;
+    }
+
+    .completedItem {
+        display: inline-block;
+    }
+
+    .notCompletedItem {
+        display: inline-block;
+    }
+
+    .toDoCompleted {
+        text-decoration: line-through;
+    }
+
+    .input2 {
+        width: 20px;
+        height: 20px;
     }
 </style>
